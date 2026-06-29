89fbec19652e195278b8ec5c4b7737066f3838df
Anthony Burton <ab@anthbrtn.com>
2026-06-29 12:24:17 +0200

    added dmi talk, rbvend in gitign

--- new_site/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind()],
-  outDir: '../_site', // Output to the same directory as the current Jekyll site
-});
